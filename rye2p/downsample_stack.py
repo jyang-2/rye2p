b8b87f01cd6ddf172adefabfbb7c79ec3f135c4b
remy <jyang2@caltech.edu>
2022-12-09 18:34:00 -0800

    Add functions to downsample movie and timestamps simultaneously

--- rye2p/downsample_stack.py
+++ rye2p/downsample_stack.py
@@ -1,16 +1,13 @@
+import copy
 from pathlib import Path
 import dateparser
 import utils2p
 import shutil
 import numpy as np
 from tqdm import tqdm
-
-steps = \
-"""
-movie:
-  acqs:use 
-    
-"""
+import xarray as xr
+import tifffile
+from rye2p import convert_raw_fn_thorimage
 
 
 # Types of ThorImage files:
@@ -41,90 +38,6 @@ movie:
 #   - Has no associated ThorSync file
 #   - Convert to 3D tiff stack
 
-# %% SECTION 00:  CONVERT RAW IMAGE FILES TO TIFF STACKS
-"""
-- Find all raw image files in day's data folder
-- Compute ThorImage/ThorSync file associations based on creation times
-- Convert .raw file into .tif stack
-- Other formats include:    
-    - split .tif stacks (<4 GB, for suite2p and caiman)
-    - 
-"""
-imaging_date = '2022-03-27'
-
-RAW_DATA_DIR = Path("/media/remy/remy-storage/Remy's Dropbox Folder/HongLab @ Caltech Dropbox/Remy/"
-                    "natural_mixtures/raw_data")
-
-PROC_DATA_DIR = Path("/local/storage/Remy/natural_mixtures/processed_data")
-# %%
-
-all_meta_files = Path.rglob("Experiment.xml")
-for meta_file in all_meta_files:
-    img_meta = utils2p.Metadata(meta_file)
-    raw_file = meta_file.with_name("Image_001_001.tif")
-
-# %% SECTION 00: RUN THOR_FILES_GET_STAT.PY ON DATA DIRECTORY
-# Run script to get file creation/modification date and timestamps
-file_types = ["Image_001_001.raw",
-              "Episode001.h5",
-              "Experiment.xml",
-              "ThorRealTimeDataSettings.xml"]
-
-all_raw_files = sorted(list(RAW_DATA_DIR.joinpath(imaging_date, '1').rglob("Image_001_001.raw")))
-rel_raw_files = [item.relative_to(RAW_DATA_DIR) for item in all_raw_files]
-# %%
-for raw_file in all_raw_files:
-    # ThorImage metadata
-    meta_file = raw_file.with_name('Experiment.xml')
-    meta = utils2p.Metadata(meta_file)
-
-    # .raw --> .tif
-    stack1, = utils2p.load_raw(raw_file, meta)
-
-    # convert to relative path
-    rel_path = raw_file.relative_to(RAW_DATA_DIR)
-    print(f"\nRaw image file: {rel_path}")
-
-    # build tiff file name
-    save_name = PROC_DATA_DIR.joinpath(rel_path.with_suffix(".tif"))
-
-    #
-    save_name.parent.mkdir(parents=True, exist_ok=True)
-    print(f"Saving to: {save_name}")
-
-    # save tiff file to project directory on network drive
-    utils2p.save_img(save_name, stack1)
-    print(f"...saved successfully.\n")
-
-# %% SPLIT STACKS
-# divide .tif files for easy loading (also, necessary to run suite2p with non-bigtiff formatted .tif files)
-
-PROC_DATA_DIR = Path("/local/storage/Remy/narrow_odors/processed_data")
-frames_per_batch = 500
-tiff_files = sorted(list(PROC_DATA_DIR.joinpath(imaging_date).rglob("Image_001_001.tif")))
-
-for file in tiff_files:
-    print(f"\nFull .tif file: {file}")
-
-    STK_DIR = file.with_name("stk")
-
-    if not STK_DIR.is_dir():
-
-        print(f"splitting movie --> {STK_DIR}")
-        STK_DIR.mkdir()
-
-        # load tiff file in batches and save
-        batch_gen = utils2p.load_stack_batches(file, frames_per_batch)
-
-        for i, batch in enumerate(batch_gen):
-            save_name = STK_DIR.joinpath(f"stk_{i:03d}.tif")
-            utils2p.save_img(save_name, batch)
-            print(f"\t{save_name} saved successfully.")
-
-
-# %% DOWNSAMPLE IN TIME IF SIGNAL IS WEAK (fast-z resonance scans will likely require this)
-
-
 def split_acquisitions_to_stacks(file):
     print(f"\n---")
     print(f"loading {file}")
@@ -148,147 +61,210 @@ def split_acquisitions_to_stacks(file):
     return saved_files
 
 
-for img_file in tiff_files[1:]:
-    print(split_acquisitions_to_stacks(img_file))
-
-
-
-
-
-
-
-#%%
-
-    # %% DOWNSAMPLE IN TIME (SUM)
-
-
-# naming convention is to insert "_tsum<...>" between the file name stem and file suffix
-
-# allow downsampling only by a divisor
-allow_nondivisor = True
-
-tiff_files = sorted(list(PROC_DATA_DIR.joinpath(imaging_date).rglob("Image_001_001.tif")))
-
-dsub_time = 3
-
-for file in tiff_files:
-    print(f"\nFull .tif file: {file}")
-
-    stack1 = utils2p.load_img(file, memmap=True)
-    n_frames = stack1.shape[0]
-
-    idx = np.arange(0, n_frames, dsub_time)
-
-    print(f"\t...Downsampling movie")
-    split_stacks = np.split(stack1, idx[1:])
-    ds_split_stacks = [item.sum(axis=0, keepdims=True) for item in split_stacks if item.shape[0] == dsub_time]
-    stack1_ds = np.concatenate(ds_split_stacks, axis=0)
-
-    # save to .tif file in the same folder as the original, full-length movie
-    save_name = f"{file.stem}_tsum{dsub_time:02d}.tif"
-    folder = file.with_name(f"downsampled_{dsub_time}")
-    folder.mkdir(exist_ok=True)
-
-    print(f"...Saving downsampled movie")
-    utils2p.save_img(folder.joinpath(save_name), stack1_ds.astype('uint16'))
-    print(f"\t...{save_name} saved successfully.")
-# %%
-
-tiff_files = PROC_DATA_DIR.rglob("Image_001_001.tif")
-tiff_files = sorted(list(tiff_files))
-
-# %% For each Experiment.xml file, determine what type of recording it is
+def edit_timestamps(timestamps, tsub, drop_frames=None):
+    da_timestamps = xr.DataArray(data=timestamps['stack_times'],
+                                 dims=['time'])
 
 
 # %%
+def imread_stk_dir(stk_dir):
+    tiff_files = sorted(list(stk_dir.glob("*.tif")))
 
-DATA_DIR = Path("/media/remy/remy-storage/Remy's Dropbox Folder/HongLab @ Caltech Dropbox/Remy/"
-                "natural_mixtures/raw_data/2022-02-10")
+    tiff_stacks = []
+    axes_list = []
+    ij_meta_list = []
 
-DB_PRJ_DIR = Path("/media/remy/remy-storage/Remy's Dropbox Folder/HongLab @ Caltech Dropbox/Remy/natural_mixtures")
+    for file in tiff_files:
+        print(f"loading {file}")
+        with tifffile.TiffFile(file) as tif:
+            stack0 = tif.asarray()
+            axes = tif.series[0].axes
+            imagej_metadata = tif.imagej_metadata
 
-RAW_DIR = DB_PRJ_DIR.joinpath('raw_data')
+        tiff_stacks.append(stack0)
+        axes_list.append(axes)
+        ij_meta_list.append(imagej_metadata)
 
-COPY_DIR = Path("/local/storage/Remy/natural_mixtures/processed_data")
+    return tiff_stacks, axes_list, ij_meta_list
 
-all_meta_files = sorted(list(DATA_DIR.rglob("Experiment.xml")))
-all_meta_files = list(filter(lambda x: "previews" not in x.parts, all_meta_files))
 
-saved_files = []
-for meta_file in all_meta_files:
-    meta = utils2p.Metadata(meta_file)
-    capture_mode = meta.get_metadata_value('CaptureMode', 'mode')
+def downsample_from_tiff(tiff_file, tsub, frames_to_drop=None, agg='sum'):
+    stack0 = utils2p.load_img(tiff_file, memmap=True)
 
-    # fastz
-    if capture_mode == '1':
-        print(meta_file)
-        raw_file = meta_file.with_name("Image_001_001.raw")
-        stack1, = utils2p.load_raw(raw_file, meta)
+    if frames_to_drop is not None:
+        stack_trimmed = np.delete(stack0, frames_to_drop, axis=0)
+    else:
+        stack_trimmed = stack0
 
-    elif capture_mode == '0':
-        print(meta_file)
+    if tiff_file.with_name('timestamps.npy').is_file():
+        timestamps = np.load(tiff_file.with_name('timestamps.npy'), allow_pickle=True).item()
+        ts = timestamps['stack_times']
+        timestamps_loaded = True
+    else:
+        ts = np.arange(t)
+        timestamps_loaded = False
+
+    da_stack0 = xr.DataArray(stack0,
+                             dims=dims,
+                             coords=dict(
+                                     t=ts,
+                                     z=np.arange(z),
+                                     y=np.arange(y),
+                                     x=np.arange(x),
+                                     )
+                             )
+    if frames_to_drop is not None:
+        da_stack = da_stack0.drop_isel(t=frames_to_drop)
+    else:
+        da_stack = copy.deepcopy(da_stack0)
+
+    if agg == 'sum':
+        da_stack = da_stack.coarsen(t=tsub, boundary='trim').sum()
+    elif agg == 'mean':
+        da_stack = da_stack.coarsen(t=tsub, boundary='trim').mean()
+
+    if timestamps_loaded:
+        timestamps_ds = copy.deepcopy(timestamps)
+        timestamps_ds['stack_times'] = da_stack['t'].to_numpy()
+        timestamps_ds['frames_dropped'] = frames_to_drop
+        timestamps_ds['tsub'] = tsub,
+    else:
+        timestamps_ds = None
+    return da_stack, timestamps_ds
+
+
+def downsample_from_stk_dir(stk_dir, tsub, frames_to_drop=None, agg='mean'):
+    tiff_list = sorted(list(stk_dir.glob("*.tif")))
+    stack0 = np.concatenate([tifffile.imread(x) for x in tiff_list], axis=0)
+    axes = 'TZYX'
+    dims = [item for item in str.lower(axes)]
+    t, z, y, x = stack0.shape
+
+    if stk_dir.with_name('timestamps.npy').is_file():
+        timestamps = np.load(stk_dir.with_name('timestamps.npy'), allow_pickle=True).item()
+        ts = timestamps['stack_times']
+        timestamps_loaded = True
+    else:
+        ts = np.arange(t)
+        timestamps_loaded = False
+
+    da_stack0 = xr.DataArray(stack0,
+                             dims=dims,
+                             coords=dict(
+                                     t=ts,
+                                     z=np.arange(z),
+                                     y=np.arange(y),
+                                     x=np.arange(x),
+                                     )
+                             )
+    if frames_to_drop is not None:
+        da_stack = da_stack0.drop_isel(t=frames_to_drop)
+    else:
+        da_stack = copy.deepcopy(da_stack0)
+
+    if agg == 'sum':
+        da_stack = da_stack.coarsen(t=tsub, boundary='trim').sum().astype('uint16')
+    elif agg == 'mean':
+        da_stack = da_stack.coarsen(t=tsub, boundary='trim').mean()
+        da_stack = da_stack.astype('float32')
+
+    if timestamps_loaded:
+        timestamps_ds = copy.deepcopy(timestamps)
+        timestamps_ds['stack_times'] = da_stack['t'].to_numpy()
+        timestamps_ds['frames_dropped'] = frames_to_drop
+        timestamps_ds['tsub'] = tsub,
+    else:
+        timestamps_ds = None
+    return da_stack, timestamps_ds
+
+
+def downsample_and_save_acquisition(stk_dir, tsub, frames_to_drop, agg='mean'):
+    da_stack, timestamps_ds = downsample_from_stk_dir(stk_dir, tsub, frames_to_drop=frames_to_drop, agg=agg)
+    src_dir = stk_dir.parent
+    mov_name = src_dir.name
+
+    # make new folder for downsampled acquisition
+    target_dir = src_dir.with_name(f"{mov_name}_tsub{tsub:02d}")
+    target_dir.mkdir(exist_ok=True)
+
+    stack_ds = da_stack.to_numpy()
+    utils2p.save_img(target_dir.joinpath("Image_001_001.tif"), stack_ds)
+    np.save(target_dir.joinpath('timestamps.npy'), timestamps_ds)
+
+    convert_raw_fn_thorimage.split_tiff_files(target_dir.joinpath("Image_001_001.tif"), frames_per_batch=500)
+    return target_dir
+
+
+def edit_movie_and_timestamps(file, tsub, frames_to_drop=None, agg='sum'):
+    """
+    stk_dir = Path("/local/matrix/Remy-Data/projects/odor_space_collab/processed_data_old/2019-03-06/4/_003/stk")
+
+    Args:
+        file ():
+        tsub ():
+        frames_to_drop ():
+        agg ():
+
+    Returns:
+
+    """
+    if file.is_dir():
+        stk_dir = file
+    else:
+        with tifffile.TiffFile(file) as tif:
+            stack0 = tif.asarray()
+            axes = tif.series[0].axes
+            imagej_metadata = tif.imagej_metadata
+        if axes == 'TZYX':
+            t, z, y, x = stack0.shape
+        dims = [item for item in str.lower(axes)]
+
+    if file.with_name('timestamps.npy').is_file():
+        timestamps = np.load(file.with_name('timestamps.npy'), allow_pickle=True).item()
+        ts = timestamps['stack_times']
+        timestamps_loaded = True
+    else:
+        ts = np.arange(t)
+        timestamps_loaded = False
+
+    da_stack0 = xr.DataArray(stack0,
+                             dims=dims,
+                             coords=dict(
+                                     t=ts,
+                                     z=np.arange(z),
+                                     y=np.arange(y),
+                                     x=np.arange(x),
+                                     )
+                             )
+    # drop frames
+    if frames_to_drop is not None:
+        da_stack = da_stack0.drop_isel(t=frames_to_drop)
+    else:
+        da_stack = copy.deepcopy(da_stack0)
+
+    # downsample
+    if agg == 'sum':
+        da_stack = da_stack.coarsen(t=tsub, boundary='trim').sum()
+    elif agg == 'mean':
+        da_stack = da_stack.coarsen(t=tsub, boundary='trim').mean()
+
+    if timestamps_loaded:
+        timestamps_ds = copy.deepcopy(timestamps)
+        timestamps_ds['stack_times'] = da_stack['t'].to_numpy()
+        timestamps_ds['frames_dropped'] = frames_to_drop
+        timestamps_ds['tsub'] = tsub,
+    else:
+        timestamps_ds = None
+    return da_stack, timestamps_ds
+
+
 # %%
-allow_nondivisor = True
-
-tiff_files = sorted(list(PROC_DATA_DIR.joinpath(imaging_date).rglob("Image_001_001.tif")))
-
-dsub_time = 3
-
-for file in tiff_files:
-    print(f"\nFull .tif file: {file}")
-
-    stack1 = utils2p.load_img(file, memmap=True)
-    n_frames = stack1.shape[0]
-
-    idx = np.arange(0, n_frames, dsub_time)
-
-    print(f"\t...Downsampling movie")
-    split_stacks = np.split(stack1, idx[1:])
-    ds_split_stacks = [item.sum(axis=0, keepdims=True) for item in split_stacks if item.shape[0] == dsub_time]
-    stack1_ds = np.concatenate(ds_split_stacks, axis=0)
-
-    # save to .tif file in the same folder as the original, full-length movie
-    save_name = f"{file.stem}_tsum{dsub_time:02d}.tif"
-    folder = file.with_name(f"downsampled_{dsub_time}")
-    folder.mkdir(exist_ok=True)
-
-    print(f"...Saving downsampled movie")
-    utils2p.save_img(folder.joinpath(save_name), stack1_ds.astype('uint16'))
-    print(f"\t...{save_name} saved successfully.")
-
-# #%%
-# saved_files = list(DATA_DIR.rglob("Image_001_001.tif"))
-# for file in saved_files:
-#     save_name = COPY_DIR.joinpath(file.relative_to(RAW_DIR))
-#     print(save_name)
-#     save_name.parent.mkdir(parents=True, exist_ok=True)
-#     shutil.move(str(file), str(save_name.parent))
-#
-# #%% SPLIT STACKS
-# from pathlib import Path
-# PROC_DIR = Path("/local/storage/Remy/natural_mixtures/processed_data/2022-02-10")
-#
-# tiff_files = list(PROC_DIR.rglob("Image_001_001.tif"))
-# for file in tiff_files:
-#     SAVE_DIR = file.with_name("stk")
-#
-#     # save planes
-#     if not SAVE_DIR.is_dir():
-#         SAVE_DIR.mkdir()
-#         batch_gen = utils2p.load_stack_batches(file, 1000)
-#         for i, batch in enumerate(batch_gen):
-#             utils2p.save_img(SAVE_DIR.joinpath(f"stk_{i:03d}.tif"), batch)
-
-# %%
-# elif capture_mode == '0':   # zstack
-#
-#
-#
-#     stack1, = utils2p.load_z_stack(meta_file.folder, meta)
-#     file =
-#     utils2p.save_img("ChannelA.tif", stack1)
-#     utils2p.save_img("ChannelB.tif", stack2)
-#
-#     SAVE_DIR = meta_file.relative_to(DB_PRJ_DIR.joinpath('raw_data'))
-#     raw_file = utils2p.find_raw_file(meta_file.parent)
-#     stack1, stack2 = utils2p.load_raw(raw_file, metadata)
+if __name__ == '__main__':
+    for thorimage_name in ['fn_0001']:
+        tsub = 5
+        folder_tsub = downsample_and_save_acquisition(
+                Path("/local/matrix/Remy-Data/projects/odor_space_collab/processed_data_old/2019-04-26/3")
+                .joinpath(thorimage_name, 'stk'),
+                frames_to_drop=None,
+                tsub=tsub,
+                )
