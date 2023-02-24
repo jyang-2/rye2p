8be52c3d55de76f6edae11c61704f696b44a5ec7
remy <jyang2@caltech.edu>
2023-02-23 20:06:27 -0800

    Add LICENSE.md and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='rye2p',
+        version='2023.02',
+        packages=['rye2p'],
+        url='https://github.com/jyang-2/rye2p',
+        license='',
+        author='remy',
+        author_email='jyang2@caltech.edu',
+        description='Convert and parse ThorImage and ThorSync data from 2p imaging experiments'
+        )
